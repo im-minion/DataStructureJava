a6bc05936babb52861d00dc54d2a8fa2c6a2f8f7
Vaibhav Miniyar <vaibhavminiyar@gmail.com>
2020-06-07 09:07:54 +0530

    refactored

--- 02.Arrays/TwoSum.java
+++ 02.Arrays/TwoSum.java
@@ -5,11 +5,11 @@ public class TwoSum {
     public static void main(String[] args) {
         int array[] = { 2, 7, 8, 6 };
 
-        int result[] = twoSum(array, 9);
-
+        List<Integer> result = twoSum(array, 9);
+        System.out.println(result);
     }
 
-    private static int[] twoSum(int arr[], int target) {
+    private static List<Integer> twoSum(int arr[], int target) {
         List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < arr.length - 1; i++) {
             if (target == (arr[i] + arr[i + 1])) {
@@ -17,8 +17,7 @@ public class TwoSum {
                 result.add(1 + i);
             }
         }
-        System.out.println(result);
-        return null;
+        return result;
     }
 
 }
