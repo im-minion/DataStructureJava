60565f038b53bc7852e4f0581c79245ea9d7a7b5
vaibhav_miniyar <vaibhav_miniyar@dell.com>
2020-06-07 13:13:46 +0530

    added reove and search programs

---
+++ 02.Arrays/RemoveDuplicateFromSorted.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicateFromSorted {
+    public static void main(String[] args) {
+        int array[] = { 1, 2, 3, 3, 4, 4 };
+        int count = remove(array);
+        System.out.println("Count : " + count);
+    }
+
+    private static int remove(int[] array) {
+        if (array.length == 0)
+            return 0;
+        int j = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[j]) {
+                array[++j] = array[i];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        return ++j;
+    }
+}
---
+++ 02.Arrays/SearchInsertPosition.java
@@ -0,0 +1,26 @@
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int array[] = { 1, 2, 3, 5 };
+        int elementToInsert = 4;
+        int position = getInsertPosition(array, elementToInsert);
+        System.out.println(position);
+    }
+
+    private static int getInsertPosition(int[] array, int elementToInsert) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array[mid] == elementToInsert) {
+                return mid;
+            }
+            if (mid >= elementToInsert) {
+                high = mid - 1;
+            } else if (mid < elementToInsert) {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
