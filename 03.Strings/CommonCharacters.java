3a7adf49a55fa3745c678aeee669ccc829ca4d42
im-minion <vaibhavminiyar@gmail.com>
2020-07-04 21:12:15 +0530

    Jewels and Stones in string

--- 03.Strings/CommonCharacters.java
+++ 03.Strings/CommonCharacters.java
@@ -1,22 +1,25 @@
+
+/**
+ * Given an array A of strings made only from lowercase letters, return a list
+ * of all characters that show up in all strings within the list (including
+ * duplicates). For example, if a character occurs 3 times in all strings but
+ * not 4 times, you need to include that character three times in the final
+ * answer.
+ * 
+ * You may return the answer in any order.
+ * 
+ * Example 1: Input: ["bella","label","roller"] Output: ["e","l","l"]
+ * 
+ * Example 2: Input: ["cool","lock","cook"] Output: ["c","o"]
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CommonCharacters {
-    /**
-     * Given an array A of strings made only from lowercase letters, return a list
-     * of all characters that show up in all strings within the list (including
-     * duplicates). For example, if a character occurs 3 times in all strings but
-     * not 4 times, you need to include that character three times in the final
-     * answer.
-     * 
-     * You may return the answer in any order.
-     * 
-     * Example 1: Input: ["bella","label","roller"] Output: ["e","l","l"]
-     * 
-     * Example 2: Input: ["cool","lock","cook"] Output: ["c","o"]
-     * 
-     */
+
     public static void main(String[] args) {
         String[] input = new String[] { "bella", "label", "roller" };
         List<String> commonChars = findCommonCharacters(input);
