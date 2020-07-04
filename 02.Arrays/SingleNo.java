860f00109dfa3c14d7957c20f3d875ec594b0f11
im-minion <vaibhavminiyar@gmail.com>
2020-07-04 21:12:15 +0530

    Jewels and Stones in string

--- 02.Arrays/SingleNo.java
+++ 02.Arrays/SingleNo.java
@@ -4,6 +4,7 @@ import java.util.Map;
 public class SingleNo {
     public static void main(String[] args) {
         int[] data = new int[] { 4, 1, 2, 1, 2 };
+        // only one number in input array occurs for one time, find that
         int number = findSingleNumber(data);
         System.out.println("Single Number " + number);
     }
@@ -12,15 +13,15 @@ public class SingleNo {
         if (data == null || data.length == 0)
             return -1;
         Map<Integer, Integer> holder = new HashMap<>();
-        for(int number : data ) {
-            if(holder.containsKey(number)) {
-                holder.put(number, holder.get(number)+1);
+        for (int number : data) {
+            if (holder.containsKey(number)) {
+                holder.put(number, holder.get(number) + 1);
             } else {
                 holder.put(number, 1);
             }
         }
-        for(int number: data) {
-            if(holder.get(number) == 1) {
+        for (int number : data) {
+            if (holder.get(number) == 1) {
                 return number;
             }
         }
