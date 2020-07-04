40241f09cedd393849c104ce828d163dc99bbb5b
im-minion <vaibhavminiyar@gmail.com>
2020-07-04 15:37:56 +0530

    updated backspace statement and added CommonChars in string

--- 03.Strings/BackspaceStringCompare.java
+++ 03.Strings/BackspaceStringCompare.java
@@ -1,4 +1,24 @@
 public class BackspaceStringCompare {
+    /**
+     * 
+     * Given two strings S and T, return if they are equal when both are typed into
+     * empty text editors. # means a backspace character.
+     * 
+     * Note that after backspacing an empty text, the text will continue empty.
+     * 
+     * Example 1: Input: S = "ab#c", T = "ad#c" Output: true Explanation: Both S and
+     * T become "ac".
+     * 
+     * Example 2: Input: S = "ab##", T = "c#d#" Output: true Explanation: Both S and
+     * T become "".
+     * 
+     * Example 3: Input: S = "a##c", T = "#a#c" Output: true Explanation: Both S and
+     * T become "c".
+     * 
+     * Example 4: Input: S = "a#c", T = "b" Output: false Explanation: S becomes "c"
+     * while T becomes "b".
+     *
+     */
     public static void main(String[] args) {
         System.out.println("String comparison after removal characters of backspace solution.");
         System.out.println("# represents backspace in text editor and the character before # is removed.");
---
+++ 03.Strings/CommonCharacters.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonCharacters {
+    /**
+     * Given an array A of strings made only from lowercase letters, return a list
+     * of all characters that show up in all strings within the list (including
+     * duplicates). For example, if a character occurs 3 times in all strings but
+     * not 4 times, you need to include that character three times in the final
+     * answer.
+     * 
+     * You may return the answer in any order.
+     * 
+     * Example 1: Input: ["bella","label","roller"] Output: ["e","l","l"]
+     * 
+     * Example 2: Input: ["cool","lock","cook"] Output: ["c","o"]
+     * 
+     */
+    public static void main(String[] args) {
+        String[] input = new String[] { "bella", "label", "roller" };
+        List<String> commonChars = findCommonCharacters(input);
+        for (String ch : commonChars) {
+            System.out.print(ch + " ");
+        }
+    }
+
+    private static List<String> findCommonCharacters(String[] A) {
+        List<String> commonChars = new ArrayList();
+        int minFrequencies[] = new int[26];
+        Arrays.fill(minFrequencies, Integer.MAX_VALUE);
+        for (String currentString : A) {
+            int charFrequencies[] = new int[26];
+
+            for (char c : currentString.toCharArray()) {
+                charFrequencies[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                minFrequencies[i] = Math.min(minFrequencies[i], charFrequencies[i]);
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            while (minFrequencies[i] > 0) {
+                commonChars.add("" + (char) (i + 'a'));
+                minFrequencies[i]--;
+            }
+        }
+        return commonChars;
+    }
+}
