6f5c27ee32d9037440e46b4065687b446adf53bc
im-minion <vaibhavminiyar@gmail.com>
2020-07-05 00:30:19 +0530

    toLower Case

---
+++ 03.Strings/ToLowerCase.java
@@ -0,0 +1,21 @@
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String input = "Testing IS Bad";
+        System.out.println("Enter the string to convert it to lowercase:");
+
+        String lowercase = convertToLowerCase(input);
+        System.out.println("String in lower case " + lowercase);
+    }
+
+    private static String convertToLowerCase(String input) {
+        StringBuilder sb = new StringBuilder("");
+        for (Character c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append((char) (c + 32));
+            } else {
+                sb.append(c);
+            }
+        }
+        return String.valueOf(sb);
+    }
+}
