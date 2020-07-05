5582cba9b5f62ccb01ad122d564582f8aeb7c28e
im-minion <vaibhavminiyar@gmail.com>
2020-07-05 17:59:13 +0530

    added problem statement for unique emails solution

--- 03.Strings/UniqueEmails.java
+++ 03.Strings/UniqueEmails.java
@@ -1,3 +1,21 @@
+
+/**
+ * leetCode URL: https://leetcode.com/problems/unique-email-addresses/
+ * 
+ * Every email consists of a local name and a domain name, separated by the @ sign.
+ * For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
+ * Besides lowercase letters, these emails may contain '.'s or '+'s.
+ * If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+ * If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+ * It is possible to use both of these rules at the same time.
+ * Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails? 
+ * Example 1:
+ * Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+ * Output: 2
+ * Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+ * 
+ */
+
 import java.util.HashSet;
 
 public class UniqueEmails {
@@ -14,11 +32,11 @@ public class UniqueEmails {
 
     private static HashSet<String> findUniqueEmails(String[] inputArray) {
         HashSet<String> outputSet = new HashSet<>();
-        for(String email: inputArray) {
+        for (String email : inputArray) {
             int splitPosition = email.indexOf("@");
             String localName = email.substring(0, splitPosition);
             String domainName = email.substring(splitPosition);
-            if(localName.indexOf("+") > 0) {
+            if (localName.indexOf("+") > 0) {
                 int plusPosition = localName.indexOf("+");
                 localName = localName.substring(0, plusPosition);
             }
