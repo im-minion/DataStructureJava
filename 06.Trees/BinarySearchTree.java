3e777646fbc6cb2b67748fcbef37c137547e3f9c
im-minion <vaibhavminiyar@gmail.com>
2020-08-11 20:45:52 +0530

    added remove function for BStree

--- 06.Trees/BinarySearchTree.java
+++ 06.Trees/BinarySearchTree.java
@@ -32,7 +32,49 @@ public class BinarySearchTree {
         }
     }
 
-    private void remove(int i) {
+    public BSTNode remove(BSTNode node, int item) {
+        if (node == null) {
+            return null;
+        }
+
+        if (node.data > item) {
+
+            node.left = remove(node.left, item);
+
+        } else if (node.data < item) {
+
+            node.right = remove(node.right, item);
+
+        } else {
+
+            // we have found the node that we want to remove.
+
+            if (node.left == null) {
+
+                node = node.right;
+            } else if (node.right == null) {
+
+                node = node.left;
+            } else {
+
+                node.data = digRight(node.right);
+                node.right = remove(node.right, item);
+            }
+
+        }
+
+        return node;
+    }
+
+    private int digRight(BSTNode right) {
+        // Dig as far left as possible to get the smallest value from the right subtree.
+        BSTNode current = right;
+        while (current.left != null) {
+
+            current = current.left;
+        }
+
+        return current.data;
     }
 
     private void insert(int item) {
@@ -63,31 +105,48 @@ public class BinarySearchTree {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
 
         System.out.println("Insert into the tree.");
-        binarySearchTree.insert(10);
-        System.out.println("Inserted " + 10);
         binarySearchTree.insert(50);
         System.out.println("Inserted " + 50);
-        binarySearchTree.insert(100);
-        System.out.println("Inserted " + 100);
+        binarySearchTree.insert(20);
+        System.out.println("Inserted " + 20);
+        binarySearchTree.insert(80);
+        System.out.println("Inserted " + 80);
+        binarySearchTree.insert(10);
+        System.out.println("Inserted " + 10);
+        binarySearchTree.insert(25);
+        System.out.println("Inserted " + 25);
+        binarySearchTree.insert(5);
+        System.out.println("Inserted " + 5);
+        binarySearchTree.insert(23);
+        System.out.println("Inserted " + 23);
+        binarySearchTree.insert(90);
+        System.out.println("Inserted " + 90);
         binarySearchTree.insert(85);
         System.out.println("Inserted " + 85);
-        binarySearchTree.insert(60);
-        System.out.println("Inserted " + 60);
+        binarySearchTree.insert(87);
+        System.out.println("Inserted " + 87);
+        binarySearchTree.insert(27);
+        System.out.println("Inserted " + 27);
 
-        System.out.println("Search 60");
-        System.out.println("Found " + binarySearchTree.search(60));
-        System.out.println("Search 40");
-        System.out.println("Found " + binarySearchTree.search(40));
+        System.out.println("Search 90");
+        System.out.println("Found " + binarySearchTree.search(90));
+        System.out.println("Search 25");
+        System.out.println("Found " + binarySearchTree.search(25));
 
-        System.out.println("Remove " + 60);
-        binarySearchTree.remove(60);
-        System.out.println("Search 60");
-        System.out.println("Found " + binarySearchTree.search(60));
+        System.out.println("Remove " + 10);
+        System.out.println(binarySearchTree.remove(binarySearchTree.root, 10));
+        System.out.println("Search 10");
+        System.out.println("Found " + binarySearchTree.search(10));
 
-        System.out.println("Remove " + 55);
-        binarySearchTree.remove(55);
-        System.out.println("Search 55");
-        System.out.println("Found " + binarySearchTree.search(55));
+        System.out.println("Remove " + 90);
+        System.out.println(binarySearchTree.remove(binarySearchTree.root, 90));
+        System.out.println("Search 90");
+        System.out.println("Found " + binarySearchTree.search(90));
+
+        System.out.println("Remove " + 10);
+        System.out.println(binarySearchTree.remove(binarySearchTree.root, 10));
+        System.out.println("Search 10");
+        System.out.println("Found " + binarySearchTree.search(10));
     }
 
 }
