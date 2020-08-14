97365a5989026aac03c8526e546f7779ba6d0236
vaibhav_miniyar <vaibhav_miniyar@dell.com>
2020-08-15 00:33:28 +0530

    test

--- 06.Trees/BinaryTreeCompleteness.java
+++ 06.Trees/BinaryTreeCompleteness.java
@@ -4,6 +4,8 @@ import java.util.Queue;
 public class BinaryTreeCompleteness {
     public static void main(String[] args) {
         
+
+        
     }
 
      public static boolean isCompleteTree(BTNode root) {
