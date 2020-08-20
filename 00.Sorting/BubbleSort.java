31aca1cad2353967d54a59e1b8dd0d6dddf1fa83
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-08-20 23:49:06 +0530

    Bubble sort details addded

--- 00.Sorting/BubbleSort.java
+++ 00.Sorting/BubbleSort.java
@@ -1,3 +1,23 @@
+/**
+ * 
+ * IMPORT THINGS
+ * 
+ * Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array
+ * is reverse sorted.
+ * 
+ * Best Case Time Complexity: O(n). Best case occurs when array is already
+ * sorted.
+ * 
+ * Auxiliary Space: O(1)
+ * 
+ * Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are
+ * already sorted.
+ * 
+ * Sorting In Place: Yes
+ * 
+ * Stable: Yes
+ */
+
 public class BubbleSort {
     public static void main(String[] args) {
         int[] inputArray = { 5, 8, 4, 6, 9, 3, 1, 2, 7 };
