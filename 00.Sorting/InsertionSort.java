8a64663b4199a3b9ea2edac07b7bfa6eab9d8b1d
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-08-21 19:13:40 +0530

    added wiki for insertion sort

--- 00.Sorting/InsertionSort.java
+++ 00.Sorting/InsertionSort.java
@@ -1,3 +1,27 @@
+/**
+ * 
+ * Time Complexity: O(n*2)
+ * 
+ * Auxiliary Space: O(1)
+ * 
+ * Boundary Cases: Insertion sort takes maximum time to sort if elements are
+ * sorted in reverse order. And it takes minimum time (Order of n) when elements
+ * are already sorted.
+ * 
+ * Algorithmic Paradigm: Incremental Approach
+ * 
+ * Sorting In Place: Yes
+ * 
+ * Stable: Yes
+ * 
+ * Online: Yes
+ * 
+ * Uses: Insertion sort is used when number of elements is small. It can also be
+ * useful when input array is almost sorted, only few elements are misplaced in
+ * complete big array.
+ * 
+ */
+
 public class InsertionSort {
     public static void main(String[] args) {
         int[] inputArray = { 5, 8, 4, 6, 9, 3, 1, 2, 7 };
