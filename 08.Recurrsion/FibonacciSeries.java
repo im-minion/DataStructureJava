5a0bb18d5fe7e6ed6d24efd0fa1ede1a94cb5738
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-08-25 19:51:55 +0530

    fibonacci modified

--- 08.Recurrsion/FibonacciSeries.java
+++ 08.Recurrsion/FibonacciSeries.java
@@ -1,13 +1,33 @@
+/**
+ * 
+ * for FibonacciSeries the constrain
+ * 
+ * the Nth element in fibonacci series will be
+ * 
+ * 1 if N<=1
+ * 
+ * 
+ */
+
 public class FibonacciSeries {
     public static void main(String[] args) {
-        int i = 6;
-        System.out.println("Number " + i);
-        System.out.println(getFibonacciForNumber(i));
+        System.out.println("Number at index" + " 6 " + " is :" + getFibonacciForNumber(6));
+
+        System.out.println("Number at index" + " 5 " + " is :" + getFibonacciForNumber(5));
+
+        printFibonacciSeriesTill(10);
+    }
+
+    private static void printFibonacciSeriesTill(int n) {
+        System.out.println("Printing all element in Fibonacci series till index : " + n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(getFibonacciForNumber(i) + " ");
+        }
     }
 
     private static int getFibonacciForNumber(int i) {
         if (i <= 1) {
-            return i;
+            return 1;
         }
         int n = getFibonacciForNumber(i - 1) + getFibonacciForNumber(i - 2);
         return n;
