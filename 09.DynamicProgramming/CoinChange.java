9a96d3380f93d8f6c1cbe173b2f12707671b17b7
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-08-28 20:58:27 +0530

    done with DP

--- 09.DynamicProgramming/CoinChange.java
+++ 09.DynamicProgramming/CoinChange.java
@@ -2,19 +2,19 @@ import java.util.Arrays;
 
 public class CoinChange {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 5 };
+        int[] coins = { 1, 2, 5 };
         int amount = 11;
-        System.out.println(coinChange(arr, amount));
+        System.out.println(coinChange(coins, amount));
     }
 
-    private static int coinChange(int[] arr, int amount) {
+    private static int coinChange(int[] coins, int amount) {
         int dp[] = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 0; i <= amount; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] <= i) {
-                    dp[i] = Math.min(dp[i], 1 + arr[i - j]);
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
                 }
             }
         }
