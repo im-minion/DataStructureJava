859cc8adc0addfa02be8b6c089caaf6b5b7f09bc
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-08-28 20:40:38 +0530

    added StairCase DP

---
+++ 09.DynamicProgramming/StairCase.java
@@ -0,0 +1,19 @@
+public class StairCase {
+    public static void main(String[] args) {
+        // System.out.println(climbStairs(0));
+        System.out.println(climbStairs(1));
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(4));
+
+    }
+
+    static int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
