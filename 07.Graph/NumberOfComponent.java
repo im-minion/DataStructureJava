8ded9dd679c48127f74eda4af5903b868360ec2f
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-08-30 00:34:44 +0530

    refacrored

--- 07.Graph/NumberOfComponent.java
+++ 07.Graph/NumberOfComponent.java
@@ -30,21 +30,27 @@ public class NumberOfComponent {
 
     public static void main(String[] args) {
         Graph graph = new Graph();
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-        graph.nodeLookup.put(1, n1);
-        graph.nodeLookup.put(2, n2);
-        graph.nodeLookup.put(3, n3);
-        graph.nodeLookup.put(4, n4);
+        graph.nodeLookup.put(1, new Node(1));
+        graph.nodeLookup.put(2, new Node(2));
+        graph.nodeLookup.put(3, new Node(3));
+        graph.nodeLookup.put(4, new Node(4));
         graph.nodeLookup.put(5, new Node(5));
         graph.nodeLookup.put(6, new Node(6));
 
         graph.addEdge(1, 2);
         graph.addEdge(2, 3);
         graph.addEdge(3, 4);
-        graph.addEdge(4, 2);
+        graph.addEdge(4, 6);
+        /**
+         *  1 - 2  
+         *      |
+         *  4 - 3
+         *  |
+         *  6   5
+         * 
+         * 1st component is => 1-2-3-4-6 
+         * 2nd component is => 5 
+         */
 
         System.out.println(getComponentsCount(graph));
     }
