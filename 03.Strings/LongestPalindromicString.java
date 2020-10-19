849c919f91379fe843f92c3c79455258ee924a42
im-minion <vaibhavminiyar@gmail.com>
2020-10-19 22:12:47 +0530

    sliding window pattern Longest palindromic string :p too hard

--- 03.Strings/LongestPalindromicString.java
+++ 03.Strings/LongestPalindromicString.java
@@ -1,6 +1,6 @@
 /**
  * Longest Palindromic Substring
- * 
+ * (Sliding Window Pattern)
  * Given a string s, find the longest palindromic substring in s. You may assume
  * that the maximum length of s is 1000.
  * 
