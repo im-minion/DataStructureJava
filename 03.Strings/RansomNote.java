abdbaefdf9dda56c9002bf5258dd8dd7b898a9b5
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-11-05 22:37:52 +0530

    RansomeNote added

---
+++ 03.Strings/RansomNote.java
@@ -0,0 +1,10 @@
+public class RansomNote {
+    public static void main(String[] args) {
+        boolean op = canConstruct("a", "b");
+        System.out.println(op);
+    }
+
+    private static boolean canConstruct(String ransomString, String magazineString) {
+        return false;
+    }
+}
