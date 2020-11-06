96ddb98134a171a3aff89a6ddf33fdf416e89758
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2020-11-06 22:32:09 +0530

    done with ransome note ...having a tedious day :exhausted:

--- 03.Strings/RansomNote.java
+++ 03.Strings/RansomNote.java
@@ -2,9 +2,21 @@ public class RansomNote {
     public static void main(String[] args) {
         boolean op = canConstruct("a", "b");
         System.out.println(op);
+        System.out.println(canConstruct("aa", "ab"));
+        System.out.println(canConstruct("aa", "aab"));
+        System.out.println(canConstruct("abcd", "abcd"));
     }
 
     private static boolean canConstruct(String ransomString, String magazineString) {
-        return false;
+        int arr[] = new int[26];
+        for (int i = 0; i < magazineString.length(); i++) {
+            arr[magazineString.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomString.length(); i++) {
+            if (--arr[ransomString.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
