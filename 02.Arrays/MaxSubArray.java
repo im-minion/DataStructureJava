4dcd232c83273d5833bdbb0ea36db216faef5398
im-minion <vaibhavminiyar@gmail.com>
2021-01-24 21:37:22 +0530

    added MaxSubArray

---
+++ 02.Arrays/MaxSubArray.java
@@ -0,0 +1,22 @@
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] Arr = {1, 2, 3, 4, -10};
+        int result = maxSubArray(Arr);
+        System.out.println(result);
+    }
+
+    public static int maxSubArray(final int[] A) {
+        int m = 0;
+        int maxSoFar = Integer.MIN_VALUE;
+        for (int j : A) {
+            m = m + j;
+            if (m < j) {
+                m = j;
+            }
+            if (maxSoFar < m) {
+                maxSoFar = m;
+            }
+        }
+        return maxSoFar;
+    }
+}
