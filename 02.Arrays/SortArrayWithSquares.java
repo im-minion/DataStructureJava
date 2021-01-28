18e510eb0bfd07d2b204d405d82c745132d16ba8
im-minion <vaibhavminiyar@gmail.com>
2021-01-28 22:48:16 +0530

    added Sort Array With Squares

---
+++ 02.Arrays/SortArrayWithSquares.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortArrayWithSquares {
+    public static void main(String[] args) {
+        ArrayList<Integer> x = new ArrayList<>();
+        x.add(-2);
+        x.add(5);
+        x.add(3);
+        ArrayList<Integer> result = solve(x);
+        for (Integer r : result) {
+            System.out.println(r);
+        }
+    }
+
+    public static ArrayList<Integer> solve(ArrayList<Integer> A) {
+        ArrayList<Integer> X = new ArrayList<>();
+        for (Integer a : A) {
+            X.add(Math.abs(a));
+        }
+        Collections.sort(X);
+        int count = 0;
+        for (Integer x : X) {
+            X.set(count++, x * x);
+        }
+
+        return X;
+    }
+}
