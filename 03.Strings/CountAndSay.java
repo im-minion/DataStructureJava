890f6b153049a37028d963b5335ad7ff16f9cfdd
im-minion <vaibhavminiyar@gmail.com>
2021-02-17 23:40:02 +0530

    updated CountAndSay.java

--- 03.Strings/CountAndSay.java
+++ 03.Strings/CountAndSay.java
@@ -3,6 +3,7 @@ public class CountAndSay {
         int test = 5;
         String output = countAndSay(test);
         System.out.println(output);
+//         1, 11, 21, 1211, 111221, 312211
     }
 
     private static String countAndSay(int n) {
@@ -27,5 +28,30 @@ public class CountAndSay {
             curr.append(count).append(say);
         }
         return curr.toString();
+    }
+
+    private static String countAndSayIDidIt(int n) {
+        if(n == 1) return "1";
+        if(n == 2) return "11";
+        String prev = "1";
+
+        StringBuilder result = new StringBuilder("11");
+        for(int i = 3 ; i <= n; i++) {
+            prev = result.toString();
+            result = new StringBuilder();
+            int count = 1;
+            String say = String.valueOf(prev.charAt(0));
+            for(int j = 1; j < prev.length(); j++) {
+                if(prev.charAt(j-1) == prev.charAt(j)) {
+                    count++;
+                } else {
+                    result.append(count).append(say);
+                    say = String.valueOf(prev.charAt(j));
+                    count = 1;
+                }
+            }
+            result.append(count).append(say);
+        }
+        return result.toString();
     }
 }
