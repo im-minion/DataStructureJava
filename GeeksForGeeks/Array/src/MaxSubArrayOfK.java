b8dffc406a37160327e2d7140ebed6053cdfd34e
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-02-24 23:48:09 +0530

    added sliding window MaxSubArrayOfK

---
+++ GeeksForGeeks/Array/src/MaxSubArrayOfK.java
@@ -0,0 +1,28 @@
+public class MaxSubArrayOfK {
+    public static void main(String[] args) {
+        int[] Arr = {1, 12, 3, 4, 10};
+        int result = solve(Arr, 2);
+        System.out.println(result);
+    }
+
+    public static int solve(int[] arr, int k) {
+        int result = Integer.MIN_VALUE;
+        int i = 0;
+        int j = 0;
+        int n = arr.length;
+        int sum = 0;
+        while (j < n) {
+            sum = sum + arr[j];
+            if ((j - i + 1) == k) {
+                result = Math.max(result, sum);
+                sum = sum - arr[i];
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return result;
+    }
+}
