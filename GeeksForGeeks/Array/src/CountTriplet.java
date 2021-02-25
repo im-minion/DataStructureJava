13daf4c2ba5c87f543ce3a3106970179ad2fe92f
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-02-25 22:23:50 +0530

    added CountTriplet and KadanesAlgoMaxSubArray

---
+++ GeeksForGeeks/Array/src/CountTriplet.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class CountTriplet {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2};
+        System.out.println(countTriplet(arr, arr.length));
+    }
+
+    public static int countTriplet(int[] arr, int n) {
+        int result = 0;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int a : arr) {
+            hashSet.add(a);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (hashSet.contains(arr[i] + arr[j])) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ GeeksForGeeks/Array/src/KadanesAlgoMaxSubArray.java
@@ -0,0 +1,49 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class KadanesAlgoMaxSubArray {
+    /*
+    Input:
+    N = 5
+    arr[] = {1,2,3,-2,5}
+    Output:
+    9
+    Explanation:
+    Max subarray sum is 9 of elements (1, 2, 3, -2, 5)
+
+    Input:
+    N = 4
+    arr[] = {-1,-2,-3,-4}
+    Output:
+    -1
+    Explanation:
+    Max subarray sum is -1 of element (-1)
+
+    LOOK AT EACH INDEX FIND WHAT IS MAX AT THAT INDEX.
+    */
+    public static void main(String[] args) {
+        int[] Arr = {1, 2, 3, 4, -10};
+        int result = maxSubArray(Arr);
+        System.out.println(result);
+    }
+
+    private static int maxSubArray(int[] arr) {
+        int maxSoFar = Integer.MIN_VALUE;
+
+        int currMax = 0;
+
+        int[] kadanesArray = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            currMax = currMax + arr[i];
+            if (currMax < arr[i]) {
+                currMax = arr[i];
+            }
+            kadanesArray[i] = currMax;
+            maxSoFar = Math.max(currMax, maxSoFar);
+        }
+        Arrays.stream(kadanesArray).forEach(el -> System.out.print(el + "\t"));
+        System.out.println("\n");
+        return maxSoFar;
+    }
+}
