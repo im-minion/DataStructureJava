a187f2386bb2d3bdc3b3d9ecb93741edfe402cb9
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-03 23:40:25 +0530

    added RearrangeMaxMinInSortedArray

--- GeeksForGeeks/Array/src/rearrange/RearrangeMaxMinInSortedArray.java
+++ GeeksForGeeks/Array/src/rearrange/RearrangeMaxMinInSortedArray.java
@@ -21,11 +21,57 @@ Expected time complexity: O(n).
 public class RearrangeMaxMinInSortedArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        reArrange(arr);
-//        rightRotate(arr, 0, 6);
+//        reArrange(arr);
+//        reArrangeWithExtraSpace(arr);
+        reArrangeWithoutExtraSpace(arr);
         Arrays.stream(arr).forEach(System.out::print);
     }
 
+    // time -> O(n) space O(1)
+    private static void reArrangeWithoutExtraSpace(int[] arr) {
+        int n = arr.length;
+        int minIndex = 0;
+        int maxIndex = n - 1;
+
+        int maxElement = arr[maxIndex] + 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                arr[i] = arr[i] + (arr[maxIndex] % maxElement) * maxElement;
+                maxIndex--;
+            } else {
+                arr[i] = arr[i] + (arr[minIndex] % maxElement) * maxElement;
+                minIndex++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+            arr[i] = arr[i] /= maxElement;
+    }
+
+    // time->O(n) space -> O(n)
+    private static void reArrangeWithExtraSpace(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        int j = n - 1;
+        int[] tempArr = new int[n];
+        for (int x = 0; x < n && i <= j; x += 2) {
+            tempArr[x] = arr[j];
+            if (x + 1 < n) {
+                tempArr[x + 1] = arr[i];
+            }
+            i++;
+            j--;
+        }
+        Arrays.stream(tempArr).forEach(System.out::print);
+        System.out.println();
+        i = 0;
+        for (int a : tempArr) {
+            arr[i] = a;
+            i++;
+        }
+    }
+
+    //time =>  O(n* n/2) => O(n2), space -> (O1)
     private static void reArrange(int[] arr) {
         int n = arr.length;
         int i = 0;
