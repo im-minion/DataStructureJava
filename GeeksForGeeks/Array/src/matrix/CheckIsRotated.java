96dfd1214567a8ddf767fb23ed631b6554ca782b
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-06 23:02:22 +0530

    added matrix CheckIsRotated

---
+++ GeeksForGeeks/Array/src/matrix/CheckIsRotated.java
@@ -0,0 +1,31 @@
+package matrix;
+
+public class CheckIsRotated {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {4, 1, 2, 3},
+                {3, 4, 1, 2},
+                {2, 3, 4, 1}
+        };
+        boolean result = isRotated(arr);
+        System.out.println(result);
+    }
+
+    private static boolean isRotated(int[][] arr) {
+        StringBuilder a = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            a.append(arr[0][i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            StringBuilder x = new StringBuilder();
+            for (int j = 0; j < arr[i].length; j++) {
+                x.append(arr[i][j]);
+            }
+            if (!(x.toString() + x.toString()).contains(a.toString())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
