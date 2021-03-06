d9f25d6778492ae2c29cc9f30e100bd1824c9fd5
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-06 22:20:57 +0530

    added matrix RotateImageBy90

---
+++ GeeksForGeeks/Array/src/matrix/RotateImageBy90.java
@@ -0,0 +1,34 @@
+package matrix;
+
+/*
+Input =>
+1    2    3    4
+5    6    7    8
+9    10    11    12
+
+Output =>
+9    5    1
+10    6    2
+11    7    3
+12    8    4
+*/
+
+public class RotateImageBy90 {
+    public static void main(String[] args) {
+        int[][] image = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        rotateBy90(image);
+    }
+
+    private static void rotateBy90(int[][] image) {
+        for (int i = 0; i < image[0].length; i++) {
+            for (int j = image.length - 1; j >= 0; j--) {
+                System.out.print(image[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
