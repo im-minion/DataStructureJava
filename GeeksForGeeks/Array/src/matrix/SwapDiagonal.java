af9cb4c7e4ddc4702d96bda6a9d112ac7b40f859
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-08 00:01:39 +0530

    added matrix SwapDiagonal

--- GeeksForGeeks/Array/src/matrix/SwapDiagonal.java
+++ GeeksForGeeks/Array/src/matrix/SwapDiagonal.java
@@ -1,5 +1,31 @@
 package matrix;
 
+/*
+Swap major and minor diagonals of a square matrix
+Difficulty Level : Basic
+
+Given a square matrix, swap the element of major and minor diagonals.
+
+Major Diagonal Elements of a Matrix :
+The Major Diagonal Elements are the ones that occur from Top Left of Matrix Down To Bottom Right Corner.
+The Major Diagonal is also known as Main Diagonal or Primary Diagonal.
+
+Minor Diagonal Elements of a Matrix :
+The Minor Diagonal Elements are the ones that occur from Top Right of Matrix Down To Bottom Left Corner.
+Also known as Secondary Diagonal.
+
+Example :
+
+Input :
+0 1 2
+3 4 5
+6 7 8
+
+Output :
+2 1 0
+3 4 5
+8 7 6
+*/
 public class SwapDiagonal {
     public static void main(String[] args) {
         int[][] mat = {
