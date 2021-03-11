32af99f6afcf7d585aa982a002283ef2e041f9d3
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-11 12:57:28 +0530

    added Arrays Search 🔎 > MaxTripletSum, SubArrayWithGivenSum

--- GeeksForGeeks/Array/src/misc/SubArrayWithGivenSum.java
+++ GeeksForGeeks/Array/src/misc/SubArrayWithGivenSum.java
@@ -8,30 +8,30 @@ public class SubArrayWithGivenSum {
         System.out.println(subarraySum(arr, arr.length, 15));
     }
 
-    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int sum) {
         ArrayList<Integer> result = new ArrayList<>();
         int i = 0;
         int j = 0;
-        int sum = 0;
+        int currSum = 0;
         while (j < n) {
-            sum = sum + arr[j];
-            if (sum == s) {
+            currSum = currSum + arr[j];
+            if (currSum == sum) {
                 //sout i and j
                 result.add(i + 1);
                 result.add(j + 1);
                 break;
-            } else if (sum > s) {
-                while(sum > s) {
-                    sum = sum - arr[i];
+            } else if (currSum > sum) {
+                while(currSum > sum) {
+                    currSum = currSum - arr[i];
                     i++;
                 }
-                if (sum == s) {
+                if (currSum == sum) {
                     result.add(i + 1);
                     result.add(j + 1);
                     break;
                 }
                 j++;
-            } else if (sum < s) {
+            } else if (currSum < sum) {
                 j++;
             }
         }
