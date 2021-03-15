d2d027799efa7ae58e7ea8182cdb1455e2f7ac02
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-15 22:54:51 +0530

    added LinkedList 📦➡📦➡null > LengthOfLinkedList

---
+++ GeeksForGeeks/LinkedList/src/linkedlist/LengthOfLinkedList.java
@@ -0,0 +1,35 @@
+package linkedlist;
+
+public class LengthOfLinkedList {
+    public static void main(String[] args) {
+        /* Start with the empty list */
+        LinkedList llist = new LinkedList();
+        llist.push(1);
+        llist.push(3);
+        llist.push(1);
+        llist.push(2);
+        llist.push(1);
+
+        System.out.println("Iterative :: Count of nodes is " + getCount(llist.head));
+
+        System.out.println("Recursive:: Count of nodes is " + getCount2(llist.head));
+
+
+    }
+
+    private static int getCount(Node head) {
+        int count = 0;
+        Node n = head;
+        while (n != null) {
+            count++;
+            n = n.next;
+        }
+        return count;
+    }
+
+
+    private static int getCount2(Node head) {
+        if (head == null) return 0;
+        return 1 + getCount2(head.next);
+    }
+}
