e82171843d2de2e8eee1d772c5d211aef6e3d19f
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-20 12:53:40 +0530

    added Dynamic Programming ♻ > Knapsack_0_1_TopDown

---
+++ 09.DynamicProgramming/Knapsack_0_1_TopDown.java
@@ -0,0 +1,35 @@
+public class Knapsack_0_1_TopDown {
+    static int[][] t; // [n][W]
+
+    public static void main(String[] args) {
+        int[] val = new int[]{60, 100, 120};
+        int[] wt = new int[]{10, 20, 30};
+        int W = 50;
+        int n = val.length;
+        int maxProfit = solveKnapsack(val, wt, W, n);
+        System.out.println(maxProfit);
+    }
+
+    private static int solveKnapsack(int[] val, int[] wt, int W, int n) {
+        t = new int[n + 1][W + 1];
+        // inz t => fill first col and first row with 0
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= W; j++) {
+                if (wt[i - 1] <= j) {
+                    t[i][j] = Math.max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j - 1]);
+                } else {
+                    t[i][j] = t[i - 1][j - 1];
+                }
+            }
+        }
+        return t[n][W];
+    }
+}
