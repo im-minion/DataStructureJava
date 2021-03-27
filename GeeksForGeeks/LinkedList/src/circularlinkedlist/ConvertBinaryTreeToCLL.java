6aa3c3a75367fd358f82a354ac08564848dc42cf
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-03-27 16:47:22 +0530

    added LinkedList added LinkedList ➡➡null > added ConvertBinaryTreeToCLL.java ImplementDQueueUsingCircularArray.java

---
+++ GeeksForGeeks/LinkedList/src/circularlinkedlist/ConvertBinaryTreeToCLL.java
@@ -0,0 +1,19 @@
+package circularlinkedlist;
+
+/*
+TODO: Later
+
+Convert a Binary Tree to a Circular Doubly Link List
+Difficulty Level : Hard
+Last Updated : 27 Aug, 2019
+Given a Binary Tree, convert it to a Circular Doubly Linked List (In-Place).
+
+The left and right pointers in nodes are to be used as previous and next pointers respectively in converted Circular Linked List.
+The order of nodes in List must be same as Inorder of the given Binary Tree.
+The first node of Inorder traversal must be head node of the Circular List.
+*/
+public class ConvertBinaryTreeToCLL {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ GeeksForGeeks/LinkedList/src/circularlinkedlist/ImplementDQueueUsingCircularArray.java
@@ -0,0 +1,22 @@
+package circularlinkedlist;
+/*
+TODO: Later
+Implementation of Deque using circular array
+Difficulty Level : Easy
+
+Deque or Double Ended Queue is a generalized version of Queue data structure that allows insert and delete at both ends.
+In previous post we had discussed introduction of deque. Now in this post we see how we implement deque Using circular array.
+Operations on Deque:
+Mainly the following four basic operations are performed on queue:
+insetFront(): Adds an item at the front of Deque.
+insertRear(): Adds an item at the rear of Deque.
+deleteFront(): Deletes an item from front of Deque.
+deleteRear(): Deletes an item from rear of Deque.
+In addition to above operations, following operations are also supported
+getFront(): Gets the front item from queue.
+getRear(): Gets the last item from queue.
+isEmpty(): Checks whether Deque is empty or not.
+isFull(): Checks whether Deque is full or not.*/
+
+public class ImplementDQueueUsingCircularArray {
+}
