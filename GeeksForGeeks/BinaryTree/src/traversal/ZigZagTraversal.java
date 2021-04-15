f7ddf02527c7b5be2e13d9251902d7d40fb410f1
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-04-15 19:50:50 +0530

    added added BinaryTree > ZigZagTraversal

---
+++ GeeksForGeeks/BinaryTree/src/traversal/ZigZagTraversal.java
@@ -0,0 +1,43 @@
+package traversal;
+
+import introduction.BinaryTree;
+import introduction.Node;
+
+import java.util.Stack;
+
+public class ZigZagTraversal {
+    public static void main(String[] args) {
+        /*Node Insertion*/
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        printZigZagTraversal(tree.root);
+    }
+
+    private static void printZigZagTraversal(Node root) {
+        if (root == null) return;
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+
+        s1.add(root);
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                Node n = s1.pop();
+                System.out.println(n.key);
+                if (n.left != null) s2.add(n.left);
+                if (n.right != null) s2.add(n.right);
+            }
+            while (!s2.empty()) {
+                Node n = s2.pop();
+                System.out.println(n.key);
+                if (n.right != null) s2.add(n.right);
+                if (n.left != null) s2.add(n.left);
+            }
+        }
+
+    }
+}
