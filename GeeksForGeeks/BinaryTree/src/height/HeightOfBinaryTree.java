272276444378cd1966ec3b00d473ed15e3e8daf0
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-04-16 18:25:29 +0530

    added added BinaryTree > HeightOfBinaryTree

---
+++ GeeksForGeeks/BinaryTree/src/height/HeightOfBinaryTree.java
@@ -0,0 +1,30 @@
+package height;
+
+import introduction.BinaryTree;
+import introduction.Node;
+
+public class HeightOfBinaryTree {
+    public static void main(String[] args) {
+        /*Node Insertion*/
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+
+        int op = getHeightOfBinaryTree(tree.root);
+        System.out.println(op);
+    }
+
+    private static int getHeightOfBinaryTree(Node root) {
+        if (root == null) return 0;
+        return 1 + Math.max(getHeightOfBinaryTree(root.left), getHeightOfBinaryTree(root.right));
+    }
+}
