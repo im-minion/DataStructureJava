fd6e4e848feec6782ca82052782daa80bb0d44b0
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-04-22 09:23:47 +0530

    added added BinaryTree 🌳 > YT_13_c_ConstructExpressionTreeFromPostfixNotation, YT_14_a_SerializationDeSerialization, YT_14_c_IsSubTree

---
+++ GeeksForGeeks/BinaryTree/src/construction/YT_13_c_ConstructExpressionTreeFromPostfixNotation.java
@@ -0,0 +1,48 @@
+package construction;
+
+import introduction.BinaryTree;
+import introduction.Node;
+
+// Verified
+public class YT_13_c_ConstructExpressionTreeFromPostfixNotation {
+    static int curr = 0;
+
+    public static void main(String[] args) {
+        char[] a = "ab-c+de/*".toCharArray();
+        curr = a.length - 1;
+        MyKlass myKlass = new MyKlass();
+        myKlass.pos = a.length - 1;
+        Node op = solve(a, myKlass);
+        BinaryTree.printCharInorderTraversal(op);
+    }
+
+    private static Node solve(char[] a, MyKlass myKlass) {
+        if (myKlass.pos < 0) return null;
+        Node root = new Node(a[myKlass.pos]);
+
+        if (a[myKlass.pos] == '+' || a[myKlass.pos] == '-' || a[myKlass.pos] == '*' || a[myKlass.pos] == '/' || a[myKlass.pos] == '^') {
+            myKlass.pos--;
+            root.right = solve(a, myKlass);
+            myKlass.pos--;
+            root.left = solve(a, myKlass);
+        }
+        return root;
+    }
+
+    private static class MyKlass {
+        int pos;
+    }
+
+    /*Alternate approach*/
+    private static Node solve(char[] a) { //
+        if (curr < 0) return null;
+        Node root = new Node(a[curr]);
+        if (a[curr] == '+' || a[curr] == '-' || a[curr] == '*' || a[curr] == '/' || a[curr] == '^') {
+            curr--;
+            root.right = solve(a);
+            curr--;
+            root.left = solve(a);
+        }
+        return root;
+    }
+}
---
+++ GeeksForGeeks/BinaryTree/src/construction/YT_14_c_IsSubTree.java
@@ -0,0 +1,58 @@
+package construction;
+
+import introduction.BinaryTree;
+import introduction.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// TODO : Giving StackOverflow on GeeksForGeeks.
+public class YT_14_c_IsSubTree {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+
+        tree.root.left.left = new Node(44);
+        tree.root.left.right = new Node(5);
+
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+
+        BinaryTree tree2 = new BinaryTree();
+        tree2.root = new Node(2);
+
+        tree2.root.left = new Node(4);
+        tree2.root.right = new Node(5);
+
+        boolean op = isSubtree(tree.root, tree2.root);
+        System.out.println(op);
+    }
+
+    public static boolean isSubtree(Node root, Node root1) {
+        List<Integer> op = new ArrayList<>();
+        serialize(root, op);
+        List<Integer> i = op;
+        // System.out.println(i);
+
+        op = new ArrayList<>();
+        serialize(root1, op);
+        List<Integer> j = op;
+        // System.out.println(j);
+        return Collections.indexOfSubList(i, j) != -1 || Collections.indexOfSubList(j, i) != -1;
+    }
+
+    private static void serialize(Node root, List<Integer> op) {
+        if (root == null) {
+            op.add(-1);
+            return;
+        }
+        op.add(root.key);
+        serialize(root.left, op);
+        serialize(root.right, op);
+    }
+}
