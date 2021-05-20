2995e4a57d1dad0f30cdc8bd2c3657a802e3cefb
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-05-20 10:10:43 +0530

    added BinaryTree 🌳 > YT_7_a_PairWithGivenTargetInBST

--- GeeksForGeeks/BinaryTree/src/binarysearchtree/YT_7_a_PairWithGivenTargetInBST.java
+++ GeeksForGeeks/BinaryTree/src/binarysearchtree/YT_7_a_PairWithGivenTargetInBST.java
@@ -3,6 +3,9 @@ package binarysearchtree;
 import introduction.BinaryTree;
 import introduction.Node;
 
+import java.util.Stack;
+
+// VERIFIED
 public class YT_7_a_PairWithGivenTargetInBST {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
@@ -28,6 +31,41 @@ public class YT_7_a_PairWithGivenTargetInBST {
     }
 
     private static boolean isPair(Node root, int key) {
+        if (root == null) return false;
+
+        Stack<Node> stackLeft = new Stack<>();
+        Stack<Node> stackRight = new Stack<>();
+
+        pushElements(root, stackLeft, 1);
+        pushElements(root, stackRight, 0);
+
+        while (!stackLeft.isEmpty() && !stackRight.isEmpty() && stackLeft.peek().key < stackRight.peek().key) {
+            int x = stackLeft.peek().key;
+            int y = stackRight.peek().key;
+            if (x + y == key) return true;
+
+            if ((x + y) < key) {
+                Node n = stackLeft.pop();
+                pushElements(n.right, stackLeft, 1);
+            } else {
+                Node n = stackRight.pop();
+                pushElements(n.left, stackRight, 0);
+            }
+        }
         return false;
+    }
+
+    private static void pushElements(Node root, Stack<Node> s, int dir) {
+        if (dir == 1) {
+            while (root != null) {
+                s.push(root);
+                root = root.left;
+            }
+        } else {
+            while (root != null) {
+                s.push(root);
+                root = root.right;
+            }
+        }
     }
 }
