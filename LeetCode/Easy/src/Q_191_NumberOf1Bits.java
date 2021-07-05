522a1c7057eeccd823ad7b111d7dc3b01d2edbad
vaibhav_miniyar <vaibhavminiyar@gmail.com>
2021-07-05 20:37:44 +0530

    added Q_191_NumberOf1Bits

---
+++ LeetCode/Easy/src/Q_191_NumberOf1Bits.java
@@ -0,0 +1,9 @@
+public class Q_191_NumberOf1Bits {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(00000000000000000000000000001011));
+    }
+    public static int hammingWeight(int n) {
+        System.out.println(n);
+        return 0;
+    }
+}
