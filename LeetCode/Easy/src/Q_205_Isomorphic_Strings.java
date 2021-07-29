71bf581704a6b4de857b3956421ee17c87c27f1b
im-minion <vaibhavminiyar@gmail.com>
2021-07-29 23:50:33 +0530

    added isomorphic string

---
+++ LeetCode/Easy/src/Q_205_Isomorphic_Strings.java
@@ -0,0 +1,33 @@
+/*
+* Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters.
+* No two characters may map to the same character, but a character may map to itself.
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+* */
+public class Q_205_Isomorphic_Strings {
+    public boolean isIsomorphic(String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
