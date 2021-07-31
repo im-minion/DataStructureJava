f1c1d79ac808782ce2f44cc5df0c50ec47836372
im-minion <vaibhavminiyar@gmail.com>
2021-08-01 00:04:37 +0530

    added Q_271_ContainsDuplicate

---
+++ LeetCode/Easy/src/Q_271_ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Q_271_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (!set.add(n)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
