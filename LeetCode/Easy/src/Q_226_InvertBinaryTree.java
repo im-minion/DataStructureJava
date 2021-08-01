f469cfa3b7b8ca251b06dc4b431a54e3000f1588
im-minion <vaibhavminiyar@gmail.com>
2021-08-01 20:17:33 +0530

    added Q_226_InvertBinaryTree

---
+++ LeetCode/Easy/src/Q_226_InvertBinaryTree.java
@@ -0,0 +1,19 @@
+public class Q_226_InvertBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+
+        root.right = invertTree(root.right);
+        root.left = invertTree(root.left);
+
+        return root;
+
+    }
+}
