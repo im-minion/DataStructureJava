ddb4a44af83ed3004288adf532a133fade34084d
im-minion <vaibhavminiyar@gmail.com>
2021-08-02 20:00:02 +0530

    added Q_231_PowerOfTwo

---
+++ LeetCode/Easy/src/Q_231_PowerOfTwo.java
@@ -0,0 +1,16 @@
+public class Q_231_PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        if (n % 2 != 0) return false;
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int val = (int) Math.pow(2, i);
+            if (val == n) {
+                return true;
+            } else if (val > n) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
