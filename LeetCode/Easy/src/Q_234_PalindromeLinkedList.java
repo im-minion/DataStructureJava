af647ed285d4acd96a16f7487a6135340a0e6103
im-minion <vaibhavminiyar@gmail.com>
2021-08-05 19:04:52 +0530

    added Q_234_PalindromeLinkedList

--- LeetCode/Easy/src/Q_234_PalindromeLinkedList.java
+++ LeetCode/Easy/src/Q_234_PalindromeLinkedList.java
@@ -1,56 +1,54 @@
 import java.util.Stack;
 
 public class Q_234_PalindromeLinkedList {
+    public static void main(String[] args) {
+        ListNode l = new ListNode(1);
+        l.next = new ListNode(0);
+        l.next.next = new ListNode(0);
+        System.out.println(isPalindrome(l));
+    }
+
     //not working
-    public boolean isPalindrome(ListNode head) {
-        if(head == null) return true;
-        if(head.next == null) return true;
-        if(head.next.next == null) {
-            if(head.val == head.next.val) {
-                return true;
-            }else {
-                return false;
-            }
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+        if (head.next == null) return true;
+        if (head.next.next == null) {
+            return head.val == head.next.val;
         }
-        ListNode actualHead = head;
         ListNode slow = head;
         ListNode fast = head;
         Stack<Integer> s = new Stack<>();
-        while(fast.next!= null && fast.next.next!= null) {
-            s.push(slow.val);
+        s.push(slow.val);
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
+            s.push(slow.val);
             fast = fast.next.next;
         }
 
-        if(fast.next == null ) {//odd
-            if(!s.isEmpty()) {
+        if (fast.next == null) {//odd-actual center
+            if (!s.isEmpty()) {
                 s.pop();
                 slow = slow.next;
-                while(!s.isEmpty() && slow != null) {
-                    if(slow.val != s.peek()){
+                while (!s.isEmpty() && slow != null) {
+                    System.out.println("Here im in odd");
+                    if (slow.val != s.peek()) {
                         return false;
                     }
                     slow = slow.next;
                     s.pop();
                 }
             }
-            // actual center
-
-
-        }else {//even
-            if(!s.isEmpty()) {
-                // s.pop();
-                slow = slow.next;
-                while(!s.isEmpty() && slow != null) {
-                    if(slow.val != s.peek()){
-                        return false;
-                    }
-                    slow = slow.next;
-                    s.pop();
+        } else {//even-has 2 centers
+            slow = slow.next;
+            while (!s.isEmpty() && slow != null) {
+                if (slow.val != s.peek()) {
+                    System.out.println("Here im in even");
+                    return false;
                 }
+                slow = slow.next;
+                s.pop();
             }
         }
         return true;
-
     }
 }
