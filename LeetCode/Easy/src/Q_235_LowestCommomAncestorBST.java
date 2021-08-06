605ac7dfb7423b273e7958895d9fe91eeb65965c
im-minion <vaibhavminiyar@gmail.com>
2021-08-06 11:44:54 +0530

    added Q_235_LowestCommomAncestorBST

---
+++ LeetCode/Easy/src/Q_235_LowestCommomAncestorBST.java
@@ -0,0 +1,18 @@
+public class Q_235_LowestCommomAncestorBST {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return root;
+
+        if (root == p || root == q) return root;
+
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) return root;
+        if (l == null) return r;
+        if (r == null) return l;
+        return null;
+    }
+}
