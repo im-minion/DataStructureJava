f742ce0787b5dc2cb03a74df0b870fb391f6cb6c
im-minion <vaibhavminiyar@gmail.com>
2022-02-15 11:05:35 +0530

    Refactored and imported the code on local machine

--- LeetCode/Medium/src/Q_002_AddTwoNumbers.java
+++ LeetCode/Medium/src/Q_002_AddTwoNumbers.java
@@ -1,19 +1,19 @@
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+//class ListNode {
+//    int val;
+//    ListNode next;
+//
+//    ListNode() {
+//    }
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    ListNode(int val, ListNode next) {
+//        this.val = val;
+//        this.next = next;
+//    }
+//}
 
 public class Q_002_AddTwoNumbers {
     public static void main(String[] args) {
