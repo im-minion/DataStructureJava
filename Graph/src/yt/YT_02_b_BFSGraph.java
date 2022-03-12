ed0427bbf6de276de63dbbf63eea33890fd3ecf5
im-minion <vaibhavminiyar@gmail.com>
2022-03-12 22:23:58 +0530

    added YT_02_b_BFSGraph

--- Graph/src/yt/YT_02_b_BFSGraph.java
+++ Graph/src/yt/YT_02_b_BFSGraph.java
@@ -9,7 +9,7 @@ import java.util.Queue;
  */
 public class YT_02_b_BFSGraph {
     public static void main(String[] args) {
-        // make visited tru e when you add an element inside a queue
+        // make visited true when you add an element inside a queue
     }
 
     // Function to return Breadth First Traversal of given graph.
@@ -18,21 +18,21 @@ public class YT_02_b_BFSGraph {
         ArrayList<Integer> op = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
         boolean[] vis = new boolean[V];
-        for (int i = 0 ; i < V ; i++) {
-             vis[i] = false;
+        for (int i = 0; i < V; i++) {
+            vis[i] = false;
         }
         queue.add(0);
         vis[0] = true;
 
         while (!queue.isEmpty()) {
             int t = queue.poll();
-           op.add(t);
-           for (int i = 0 ; i < adj.get(t).size() ; i ++ ) {
-               if (!vis[adj.get(t).get(i)]) {
-                   queue.add(adj.get(t).get(i));
-                   vis[adj.get(t).get(i)] = true;
-               }
-           }
+            op.add(t);
+            for (int i = 0; i < adj.get(t).size(); i++) {
+                if (!vis[adj.get(t).get(i)]) {
+                    queue.add(adj.get(t).get(i));
+                    vis[adj.get(t).get(i)] = true;
+                }
+            }
         }
 
         return op;
