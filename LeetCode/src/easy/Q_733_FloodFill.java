7a36251cab1a37c00413ea4ae0acb4a89ed7f2fa
im-minion <vaibhavminiyar@gmail.com>
2024-08-27 20:30:15 +0530

    added floodFill

---
+++ LeetCode/src/easy/Q_733_FloodFill.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class Q_733_FloodFill {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    public void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != color) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        fill(image, sr - 1, sc, color, newColor);
+        fill(image, sr + 1, sc, color, newColor);
+        fill(image, sr, sc - 1, color, newColor);
+        fill(image, sr, sc + 1, color, newColor);
+    }
+}
